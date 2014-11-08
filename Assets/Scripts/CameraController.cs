062e0a0ab1ec45475429200d155c10d415fa352b
mildmojo <mildmojo@users.noreply.github.com>
2014-11-08 15:58:21 -0500

    Adds initial CameraController and PlayerManager.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class CameraController : MonoBehaviour {
+	[HideInInspector] [NonSerializable]
+	public List<GameObject> Players = new List<GameObject>();
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GameObject frontPlayer = getFrontPlayer();
+		Camera.main.transform.LookAt(frontPlayer.transform);
+	}
+
+	GameObject getFrontPlayer() {
+		return Players.Max (player => player.GetComponent<PlayerManager>().distanceTraveled);
+	}
+}
