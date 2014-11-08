bb83e2d3a141fac090bcdceafc5fc6a6b85ded1c
John Meister <john@supersoul.co>
2014-11-08 15:18:58 -0500

    Initial stuff

---
+++ Assets/Scripts/JetStreamComponent.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JetStreamComponent : MonoBehaviour {
+
+	public Vector3 Force;
+	
+	private void OnTriggerEnter(Collider collider)
+	{
+		AddForce(collider);
+	}
+
+	private void OnTriggerStay(Collider collider)
+	{
+		AddForce(collider);
+	}
+
+	private void AddForce(Collider collider)
+	{
+		collider.attachedRigidbody.AddForce(Force);
+	}
+}
